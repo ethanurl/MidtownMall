113b9b95fd60cddbe03185d958c0967019cabeb0
ethanurl <130177026+ethanurl@users.noreply.github.com>
2023-05-11 16:37:24 -0700

    Add files via upload
    Commit 1: Made all my scenes in game.js and edited the duration on messages in adventure.js
    Created my first game scene and made the intro work.
    Gonna keep working on the next scenes 1 at a time and hope for no issues.
    Maybe I can change the background color or inventory font to feel more 'gamey'.

--- game.js
+++ game.js
@@ -1,63 +1,13 @@
-class Demo1 extends AdventureScene {
+class Navi extends AdventureScene {
     constructor() {
-        super("demo1", "First Room");
+        super("navi", `Directions! Use this area to navigate throughout the mall.`);
     }
-
+    preload() {
+        this.load.image('mallsignmain', 'mallsign.png')
+        this.load.image('spbdirect', 'directionsspb.png')
+    }
     onEnter() {
-
-        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => this.showMessage("Metal, bent."))
-            .on('pointerdown', () => {
-                this.showMessage("No touching!");
-                this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
-                });
-            });
-
-        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("It's a nice key.")
-            })
-            .on('pointerdown', () => {
-                this.showMessage("You pick up the key.");
-                this.gainItem('key');
-                this.tweens.add({
-                    targets: key,
-                    y: `-=${2 * this.s}`,
-                    alpha: { from: 1, to: 0 },
-                    duration: 500,
-                    onComplete: () => key.destroy()
-                });
-            })
-
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
-                }
-            })
-            .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
-                }
-            })
-
+        this.add.sprite(this.w * 0.375, this.w * 0.275, 'mallsignmain').setScale(2)
     }
 }
 
@@ -73,7 +23,7 @@ class Demo2 extends AdventureScene {
                 this.showMessage("You've got no other choice, really.");
             })
             .on('pointerdown', () => {
-                this.gotoScene('demo1');
+                this.gotoScene('navi');
             });
 
         let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
@@ -88,7 +38,58 @@ class Demo2 extends AdventureScene {
                     duration: 500
                 });
             })
-            .on('pointerdown', () => this.gotoScene('outro'));
+            .on('pointerdown', () => 
+            this.gotoScene('outro'));
+    }
+}
+class SPB extends AdventureScene {
+    constructor() {
+        super("spb", "Shirts, Pants, and Beyond");
+    }
+    preload() {
+        this.load.image('entrance', 'mallentrance (1).png')
+        this.load.image('mallsign', 'midtownmall.png')
+        this.load.image('doors', 'mallentrancedoors.png')
+        this.load.image('money', 'money.png')
+    }
+    onEnter() {
+        this.add.text(this.w * 0.3, this.w * 0.4, 'yo mama').setFontSize(100)
+        this.add.sprite(this.w * 0.4, this.w * 0.28, 'entrance').setScale(1.3)
+        this.add.sprite(this.w * 0.4, this.w * 0.095, 'mallsign').setScale(0.5)
+        this.add.sprite(this.w * 0.4, this.w * 0.345, 'doors').setScale(1.3)
+            .setInteractive()
+            .on('pointerover', () => {
+                if (this.hasItem('money')) {
+                    this.showMessage('Enter the Mall!');
+                } else {
+                    this.showMessage(`You don't have any money. 
+                    Maybe there's some around?`);   
+                }
+                })
+            .on('pointerdown', () => {
+                if (this.hasItem('money')) {
+                    this.gotoScene('navi');
+                } else {
+                    this.showMessage(`You don't have any money.
+                    Maybe there's some around?`)
+                }
+                })
+            let money = this.add.sprite(this.w * 0.5, this.w * 0.5, 'money').setScale(0.15)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('Get some money.');
+            })
+            .on('pointerdown', () => {
+                this.gainItem('money'),
+                this.tweens.add({
+                targets: money,
+                alpha: {from: 1, to: 0},
+                duration: 500,
+                onComplete: () => money.destroy()
+                })
+            })
+            
+
     }
 }
 
@@ -96,12 +97,23 @@ class Intro extends Phaser.Scene {
     constructor() {
         super('intro')
     }
+    preload() {
+        this.load.image('logo', 'E2Games.png')
+    }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
-        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
-        this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+        this.jimberly = this.add.text(525, 500, 'Click To Start').setFontSize(100)
+        this.image = this.add.sprite(2500, 500, 'logo');
+        this.input.once('pointerdown', () => {
+            this.jimberly.destroy()
+            this.tweens.add({
+                targets: this.image,
+                x: -500,
+                y: 500,
+                ease: 'Linear',
+                duration: 3000
+            })
+            this.time.delayedCall(3000, () => this.cameras.main.fade(1000, 0,0,0));
+            this.time.delayedCall(1, () => this.scene.start('spb'));
         });
     }
 }
@@ -125,7 +137,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Demo1, Demo2, Outro],
+    scene: [Intro, SPB, Navi, Demo2, Outro],
     title: "Adventure Game",
 });
 
